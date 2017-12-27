846643e1b470e2b0bd167d14f870322ed223b284
kvkens <kvkens@qq.com>
2017-12-27 14:50:12 +0800

    init

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello")
+/**
+ * static server plugin for uba-server
+ * Date : 2017-12-27 13:20:18
+ */
+
+const path = require("path");
+const svr = require("koa-static");
+
+module.exports = (app, opt) => {
+  app.use(svr(opt.root));
+}
